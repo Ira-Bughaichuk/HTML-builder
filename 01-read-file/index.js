27e3c4e9ff17be3a81d5bc80e1b77fa02f9dcc94
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2024-01-17 21:45:10 +0200

    feat: add 1 task

---
+++ 01-read-file/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+const rr = fs.createReadStream(path.join(__dirname, 'text.txt'), { encoding: 'utf-8' });
+rr.on('data', (chunk) => {
+    console.log(chunk.toString());
+  });
+
+  rr.on('error', (err) => {
+    console.error(`Error reading file: ${err}`);
+  }); 
+
+  rr.on('end', () => {
+    console.log('Reading finished');
+  }); 
+
+
+// 1- variant
+/*fs.readFile(path.join(__dirname, 'text.txt'), 'utf-8', (err, data) => {
+    if(err) throw err;
+    console.log(data);
+})
+
+// Обробка неперехоплених виключень (uncaughtExceptions)
+process.on('uncaughtException', err => {
+    console.error(`Uncaught Exception: ${err}`);
+    process.exit(1);
+});
+*/
