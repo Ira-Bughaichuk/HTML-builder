d2c450c24ac4d6f6ac860e0e82432d1f6e359166
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2024-01-18 18:32:17 +0200

    feat: done 4 task

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,46 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+async function copyFileSync(source, target) {
+    const data = await fs.readFile(source);
+    await fs.writeFile(target, data);
+}
+
+async function copyFolderAsync(source, target) {
+    try {
+        await fs.mkdir(target);
+    } catch (err) {
+        if (err.code !== 'EEXIST') {
+            throw err;
+        }
+    }
+
+    const files = await fs.readdir(source);
+
+    for (const file of files) {
+        const currentSource = path.join(source, file);
+        const currentTarget = path.join(target, file);
+
+        const stats = await fs.stat(currentSource);
+
+        if (stats.isDirectory()) {
+            await copyFolderAsync(currentSource, currentTarget);
+        } else {
+            await copyFileSync(currentSource, currentTarget);
+        }
+    }
+}
+
+async function main() {
+    const sourcePath = path.join(__dirname, 'files');
+    const destinationPath = path.join(__dirname, 'copy-files');
+
+    try {
+        await copyFolderAsync(sourcePath, destinationPath);
+        console.log('Folder copied successfully!');
+    } catch (err) {
+        console.error(`Error copying folder: ${err}`);
+    }
+}
+
+main();
