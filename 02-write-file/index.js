9ce98223dc64a988cb28eed70eab21c9d3d7af8a
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2024-01-18 19:41:38 +0200

    refactop: fix pritier task 2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,25 +1,26 @@
 const fs = require('fs');
 const path = require('path');
 
-
-const ws = fs.createWriteStream(path.join(__dirname, 'new-text.txt'), { flags: 'a' });
-const rs = fs.createReadStream(path.join(__dirname, 'new-text.txt'), { encoding: 'utf-8' });
+const ws = fs.createWriteStream(path.join(__dirname, 'new-text.txt'), {
+  flags: 'a',
+});
+const rs = fs.createReadStream(path.join(__dirname, 'new-text.txt'), {
+  encoding: 'utf-8',
+});
 
 ws.write('Hello, it is new text ! ');
 ws.write('All are good now !');
 ws.end();
 
-
 rs.on('data', (chunk) => {
-    console.log(chunk.toString());
+  console.log(chunk.toString());
 });
 
 rs.on('error', (err) => {
-    console.error(`Error reading file: ${err}`);
-}); 
+  console.error(`Error reading file: ${err}`);
+});
 
 rs.on('end', () => {
-    console.log('Writing finished');
-}); 
+  console.log('Writing finished');
+});
 
-  // rr.pipe(ws);
