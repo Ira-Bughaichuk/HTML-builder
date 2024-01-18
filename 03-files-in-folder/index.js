0a52d56591d289bf2bfdbf495d4bb35e374da726
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2024-01-18 16:54:49 +0200

    done 3 task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const fs = require('fs').promises;
+
+const directoryPath = path.join(__dirname, './secret-folder'); //отримати імена всіх файлів у вказаному каталозі.
+
+async function readDirectory() {
+    try {
+      const files = await fs.readdir(directoryPath, { withFileTypes: true });
+      for (const file of files) {
+        const fileName = file.name;
+        const isFile = file.isDirectory(); // чи є об'єкт файлом.
+        if(!isFile){
+            const typeFile = path.extname(fileName);  //розширення файлу
+            const filePath = path.join(directoryPath, fileName);
+
+            getStats(filePath, fileName, typeFile);
+        }
+      }
+    } catch (err) {
+      console.error(err);
+    }
+}
+async function getStats(filePath, fileName, typeFile){
+    try {
+        const fileStats = await fs.stat(filePath);
+        console.log(`File ${fileName} and its expansion ${typeFile} has stats: ${JSON.stringify(fileStats, null, 2)}`);
+    } catch (err) {
+        console.error(`Error getting stats for file ${fileName}: ${err}`);
+    }
+}
+
+readDirectory();
+
